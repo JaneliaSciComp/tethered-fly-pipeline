e89272565f96154537d597a0bc5efcc8611dbeca
Cristian Goina <goinac@hhmi.org>
2022-05-18 21:57:01 -0400

    set mcr_root

--- containers/apt_track/scripts/entrypoint.sh
+++ containers/apt_track/scripts/entrypoint.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+if [ -d /scratch ] ; then
+  export MCR_CACHE_ROOT="/scratch/${USER}/mcr_cache_$$"
+else
+  export MCR_CACHE_ROOT=`mktemp -u`
+fi
+
+echo "User ${USER}"
+echo "Use MCR_CACHE_ROOT ${MCR_CACHE_ROOT}"
+
+[ -d ${MCR_CACHE_ROOT} ] || mkdir -p ${MCR_CACHE_ROOT}
+
+umask 0002
+
 bin_app="/app/bin/compute3Dfrom2D_compiled"
 echo "Run ${bin_app} ${@}"
 ${bin_app} "${@}"
