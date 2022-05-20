60de590db63d1fe93bbd24caa871d58e600e100c
Cristian Goina <goinac@hhmi.org>
2022-05-20 11:59:40 -0400

    use job id in mcr_cache_root

--- containers/apt_track/scripts/entrypoint.sh
+++ containers/apt_track/scripts/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ -d /scratch ] ; then
-  export MCR_CACHE_ROOT="/scratch/${USER}/mcr_cache_$$"
+  export MCR_CACHE_ROOT="/scratch/${USER}/apt_track/mcr_cache_${LSB_JOBID}_$$"
 else
   export MCR_CACHE_ROOT=`mktemp -u`
 fi
